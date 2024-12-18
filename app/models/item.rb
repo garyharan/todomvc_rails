83ca344b2b41b93218b20efaccf7e44ae8f4ab12
Gary Haran <garyharan@users.noreply.github.com>
2024-12-17 20:30:27 -0500

    feat: add footer count automatically broadcasted

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,13 @@
 class Item < ApplicationRecord
+  after_create_commit { broadcast_item_count }
+  after_destroy_commit { broadcast_item_count }
+
+  private
+
+  def broadcast_item_count
+    broadcast_replace_to "items",
+      target: "item_counter",
+      partial: "items/footer",
+      locals: { items: Item.all }
+  end
 end
